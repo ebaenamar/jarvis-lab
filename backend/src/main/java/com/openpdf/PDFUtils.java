82f59a89165a046eff31cb6a3fef6a134435c62b
student1919 <awesome.is.me40@gmail.com>
2026-08-02 14:44:01 -0400

    added src

---
+++ backend/src/main/java/com/openpdf/PDFUtils.java
@@ -0,0 +1,38 @@
+package com.openpdf;
+
+
+import com.lowagie.text.pdf.PdfReader;
+
+
+public class PDFUtils {
+
+
+    public static int getPageCount(
+            String file
+    ){
+
+        try {
+
+            PdfReader reader =
+                    new PdfReader(file);
+
+
+            int pages =
+                    reader.getNumberOfPages();
+
+
+            reader.close();
+
+
+            return pages;
+
+
+        }catch(Exception e){
+
+            throw new RuntimeException(e);
+
+        }
+
+    }
+
+}
